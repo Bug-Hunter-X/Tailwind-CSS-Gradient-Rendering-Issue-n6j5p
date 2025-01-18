d139b64200b11bc17ffd3cd17a0e131468d22f51
BugHunter-X <askzenai@gmail.com>
2025-01-18 05:02:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,7 @@
+```javascript
+// in your component file
+<div className="bg-gradient-to-r from-blue-500 to-purple-500 p-4 rounded-lg shadow-md">
+  {/* content */}
+</div>
+```
+This code uses Tailwind's gradient feature. However, if you encounter issues where the gradient doesn't display correctly or appears distorted, there might be a conflict with other CSS rules or a problem with the browser's rendering engine.
